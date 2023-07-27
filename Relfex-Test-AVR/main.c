c8028096c001f4b2f3204399e5c0286a8a149653
Ali <ali.mirghasemi1376@gmail.com>
2023-07-27 19:49:42 +0330

    Initial commit

---
+++ Relfex-Test-AVR/main.c
@@ -0,0 +1,18 @@
+/*
+ * Relfex-Test-AVR.c
+ *
+ * Created: 7/27/2023 7:46:05 PM
+ * Author : MrCrazy
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
